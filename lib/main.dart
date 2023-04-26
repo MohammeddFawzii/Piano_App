4730a2cb3feb39df5c027f5fa4a2de0190e21cc7
MohammeddFawzii <fawzimohamed@gmail.com>
2023-04-26 15:05:01 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PianoApp());
+}
+
+class PianoApp extends StatelessWidget {
+  const PianoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
